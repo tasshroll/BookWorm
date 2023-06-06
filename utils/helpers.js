5078a4343024ce9d104491f7735ea69d2cd16711
Shannon <shogeboom187@gmail.com>
2023-06-05 21:07:49 -0700

    deleted format_date from profile.hbs

--- utils/helpers.js
+++ utils/helpers.js
@@ -5,7 +5,7 @@ module.exports = {
   },
   format_amount: (amount) => {
     // format large numbers with commas
-    return parseInt(amount).toLocaleString();
+    return parseInt(amount).toLocaleDateString();
   },
   get_emoji: () => {
     const randomNum = Math.random();
